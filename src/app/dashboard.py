a17db7fa93cd3b77dc41a80d22b642c9c143ba1e
areebbinnadeem <m.areebbinnadeem@gmail.com>
2025-01-21 00:25:22 +0500

    Deploying on Streamlit Cloud

--- src/app/dashboard.py
+++ src/app/dashboard.py
@@ -1,3 +1,7 @@
+import sys
+import os
+# Add the parent directory to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
 import os
 import pandas as pd
 import streamlit as st
